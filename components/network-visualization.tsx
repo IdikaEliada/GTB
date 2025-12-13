74e9cbad6ba94b8065a127318c3624059b3478f9
neuralnex <omezirizion@gmail.com>
2025-12-13 05:36:38 -0800

    push

--- components/network-visualization.tsx
+++ components/network-visualization.tsx
@@ -5,13 +5,15 @@ export function NetworkVisualization() {
     <section className="w-full py-16 px-6 bg-background">
       <div className="max-w-6xl mx-auto">
         <div className="rounded-2xl overflow-hidden">
-          <Image
-            src="/new%20images/right.jpg"
-            alt="The right opportunity is here"
-            width={1200}
-            height={600}
-            className="w-full h-auto object-cover"
-          />
+          <div className="w-full h-[500px] md:h-[600px] overflow-hidden">
+            <Image
+              src="/new%20images/right.jpg"
+              alt="The right opportunity is here"
+              width={1200}
+              height={800}
+              className="w-full h-full object-cover object-top"
+            />
+          </div>
         </div>
       </div>
     </section>
