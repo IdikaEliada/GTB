cdbe1070419c15bdbf1e89f6c273887cf394759b
IdikaEliada <idika1996@gmail.com>
2025-12-16 21:22:06 +0100

    added the id

--- components/features-section.tsx
+++ components/features-section.tsx
@@ -72,7 +72,7 @@ export function FeaturesSection() {
 
 export default function FeaturesSection2() {
   return (
-    <section className="w-full py-20 px-6">
+    <section className="w-full py-20 px-6" id="why">
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md mb-8 lg:mb-16">
           <a
